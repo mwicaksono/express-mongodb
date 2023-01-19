bcb9b36c0774ae265204a9d9e454ac262340801f
Masan Abdi Wicaksono <masan.wicaksono@ptsci.id>
2023-01-19 12:56:33 +0700

    add detail, delete function

--- routes/blog.js
+++ routes/blog.js
@@ -7,14 +7,40 @@ router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const posts = await db.getDb().collection('posts').find().toArray();
+  res.render('posts-list', { posts });
 });
 
 router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
   res.render('create-post', { authors });
 });
+
+router.get('/posts/:id', async (req, res) => {
+  const postId = new ObjectId(req.params.id);
+  const post = await db.getDb().collection('posts').findOne({ _id: postId })
+  res.render('post-detail', { post });
+})
+
+router.get('/posts/:id/edit', async (req, res) => {
+  const postId = new ObjectId(req.params.id);
+  const post = await db.getDb().collection('posts').findOne({ _id: postId })
+  res.render('update-post', { post });
+})
+
+router.post('/posts/:id/delete', async (req, res) => {
+  const postId = new ObjectId(req.params.id);
+  const result = await db.getDb().collection('posts').deleteOne({ _id: postId });
+  console.log(result);
+  res.redirect('/posts');
+})
+
+// router.post('/posts/:id/update', async (req, res) => {
+//   const postId = new ObjectId(req.params.id);
+//   const result = await db.getDb().collection('posts').updateOne({ _id: postId });
+//   console.log(result);
+// })
 
 router.post('/posts', async (req, res) => {
   const authorId = new ObjectId(req.body.author);
